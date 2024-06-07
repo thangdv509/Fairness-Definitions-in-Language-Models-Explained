c529e00a01b9c578d3e74da5c51c7a406c249ad2
Doan Viet Thang <thang.dv509@gmail.com>
2024-06-08 00:56:52 +0700

    first commit

---
+++ ceat/ceat.py
@@ -0,0 +1,292 @@
+import numpy as np
+from sklearn.metrics.pairwise import cosine_similarity
+import pickle
+import scipy.stats
+import time as t
+import pickle
+import random
+import datetime
+
+
+
+
+# weat 1
+flowers = ['aster', 'clover', 'hyacinth', 'marigold', 'poppy', 'azalea', 'crocus', 'iris', 'orchid', 'rose', 'bluebell', 'daffodil', 'lilac', 'pansy', 'tulip', 'buttercup', 'daisy', 'lily', 'peony', 'violet', 'carnation', 
+'magnolia', 'petunia', 'zinnia','gladiola'] #'gladiola' deleted since it not appear
+insects = ['ant', 'caterpillar', 'flea', 'locust', 'spider', 'bedbug', 'centipede', 'fly', 'maggot', 'tarantula',
+'bee', 'cockroach', 'gnat', 'mosquito', 'termite', 'beetle', 'cricket', 'hornet', 'moth', 'wasp', 
+'dragonfly', 'horsefly', 'roach', 'weevil','blackfly'] # 'blackfly' deleted for sysmetric since it only appears 1 time.
+pleasant = ['caress', 'freedom', 'health', 'love', 'peace', 'cheer', 'friend', 'heaven', 'loyal', 'pleasure', 'diamond', 'gentle', 'honest', 
+'lucky', 'rainbow', 'diploma', 'gift', 'honor', 'miracle', 'sunrise', 'family',
+'happy', 'laughter', 'paradise', 'vacation']
+unpleasant = ['abuse', 'crash', 'filth', 'murder', 'sickness', 'accident', 'death', 'grief', 'poison', 'stink',
+'assault', 'disaster', 'hatred', 'pollute', 'tragedy', 'divorce', 'jail', 'poverty', 'ugly', 'cancer', 'kill', 'rotten',
+'vomit', 'agony', 'prison']
+
+#weat 2
+instruments = ['cello', 'guitar', 'lute', 'trombone', 'banjo', 'clarinet', 'harmonica', 'mandolin',
+'trumpet', 'bassoon', 'drum', 'harp', 'oboe', 'tuba', 'bell', 'fiddle', 'harpsichord', 'piano', 'viola', 'bongo',
+'flute', 'horn', 'saxophone', 'violin']
+weapons = ['arrow', 'club', 'gun', 'missile', 'spear', 'axe', 'dagger', 'harpoon', 'pistol', 'sword', 'blade',
+'dynamite', 'hatchet', 'rifle', 'tank', 'bomb', 'firearm', 'knife', 'shotgun', 'teargas', 'cannon', 'grenade',
+'mace', 'slingshot', 'whip']
+pleasant, unpleasant
+
+#weat 3
+#weat 3
+european_3 = ['Adam', 'Harry', 'Roger', 'Alan', 
+'Ryan', 'Andrew',  'Jack', 'Matthew', 'Stephen', 'Brad', 'Greg' , 'Paul', 
+'Jonathan', 'Peter',  'Amanda', 'Courtney',  'Melanie', 'Katie', 'Kristin', 'Nancy', 'Stephanie', 
+'Ellen', 'Lauren', 'Colleen', 'Emily', 'Megan', 'Rachel','Betsy','Justin','Frank','Josh','Heather'] #delte random: 'Betsy','Justin','Frank','Josh','Heather'
+
+african_3 = [ 'Alonzo',   'Theo', 'Alphonse', 'Jerome',
+'Leroy',  'Torrance', 'Darnell', 'Lamar', 'Lionel', 'Tyree', 'Deion', 'Lamont', 'Malik',
+'Terrence', 'Tyrone',  'Lavon', 'Marcellus',  'Wardell', 'Nichelle',
+ 'Ebony',  'Shaniqua',  'Jasmine',
+'Tanisha', 'Tia',  'Latoya', 'Yolanda',  'Yvette','Jamel','Shereen', 'Latisha','Lakisha','Malika'] #delete 'Jamel','Shereen', 'Latisha','Lakisha','Malika',  because not appear
+pleasant_3 = [ 'caress', 'freedom', 'health', 'love', 'peace', 'cheer', 'friend', 'heaven', 'loyal', 'pleasure', 
+'diamond', 'gentle', 'honest', 'lucky', 'rainbow', 'diploma', 'gift', 'honor', 'miracle', 'sunrise', 'family', 'happy', 'laughter', 'paradise', 'vacation']
+
+unpleasant_3 = ['abuse', 'crash', 'filth', 'murder', 'sickness', 'accident', 'death', 'grief', 'poison', 'stink', 'assault', 'disaster', 
+'hatred', 'pollute', 'tragedy', 'bomb', 'divorce', 'jail', 'poverty', 'ugly', 'cancer', 'evil', 'kill', 'rotten', 'vomit'] 
+
+weat2 = instruments + weapons + pleasant + unpleasant
+weat3 = european_3 + african_3 + pleasant_3 + unpleasant_3
+# bomb, evil in 3 not in 1, agony prison in 1 not in 3. #pleasant 3 = pleasant 1
+
+#weat 4
+european_4 = ['Brad', 'Brendan', 'Geoffrey', 'Greg', 'Brett',  'Matthew', 
+'Todd', 'Allison', 'Anne', 'Carrie', 'Emily', 'Jill', 'Laurie',  'Meredith', 'Sarah','Neil'] #delete 'Neil' sysmetric
+african_4 = ['Darnell', 'Hakim', 'Jermaine', 'Kareem', 'Jamal', 'Leroy', 'Rasheed',
+ 'Tyrone', 'Aisha', 'Ebony', 'Keisha', 'Kenya',   'Latoya', 'Tamika',
+'Tanisha','Lakisha'] #delete Lakisha because not appear
+pleasant_3, unpleasant_3
+
+#weat 5
+european_4, african_4
+pleasant_5 = ['joy', 'love', 'peace', 'wonderful', 'pleasure', 'friend', 'laughter', 'happy']
+unpleasant_5 = ['agony', 'terrible', 'horrible', 'nasty', 'evil', 'war', 'awful', 'failure']
+
+#weat  6
+male = ['John', 'Paul', 'Mike', 'Kevin', 'Steve', 'Greg', 'Jeff', 'Bill']
+female = ['Amy', 'Joan', 'Lisa', 'Sarah', 'Diana', 'Kate', 'Ann', 'Donna']
+career = ['executive', 'management', 'professional', 'corporation', 'salary', 'office', 'business', 'career']
+family = ['home', 'parents', 'children', 'family', 'cousins', 'marriage', 'wedding', 'relatives']
+
+#weat 7
+math = ['math', 'algebra', 'geometry', 'calculus', 'equations', 'computation', 'numbers', 'addition']
+arts = ['poetry', 'art', 'dance', 'literature', 'novel', 'symphony', 'drama', 'sculpture']
+male_term = ['male', 'man', 'boy', 'brother', 'he', 'him', 'his', 'son']
+female_term = ['female', 'woman', 'girl', 'sister', 'she', 'her', 'hers', 'daughter']
+
+#weat 8
+science = ['science', 'technology', 'physics', 'chemistry', 'Einstein', 'NASA', 'experiment', 'astronomy']
+arts_8 = ['poetry', 'art', 'Shakespeare', 'dance', 'literature', 'novel', 'symphony', 'drama']
+male_term_8 = ['brother', 'father', 'uncle', 'grandfather', 'son', 'he', 'his', 'him']
+female_term_8 = ['sister', 'mother', 'aunt', 'grandmother', 'daughter', 'she', 'hers', 'her']
+
+#weat 9
+mental_disease = ['sad', 'hopeless', 'gloomy', 'tearful', 'miserable', 'depressed']
+physical_disease = ['sick', 'illness', 'influenza', 'disease', 'virus', 'cancer']
+temporary = ['impermanent', 'unstable', 'variable', 'fleeting', 'short', 'brief', 'occasional' ]
+permanent = [ 'stable', 'always', 'constant', 'persistent', 'chronic', 'prolonged', 'forever']
+
+#weat 10
+young_name = ['Tiffany', 'Michelle', 'Cindy', 'Kristy', 'Brad', 'Eric', 'Joey', 'Billy']
+old_name = [ 'Ethel', 'Bernice', 'Gertrude', 'Agnes', 'Cecil', 'Wilbert', 'Mortimer', 'Edgar']
+pleasant_5, unpleasant_5
+
+african_female = ['Yvette','Aisha','Malika','Latisha','Keisha','Tanisha',
+'Tamika', 'Yolanda','Nichelle','Latoya','Lakisha','Shereen','Shaniqua',
+'Jasmine','Tia'][0:12]
+# delete:  Kenya
+
+african_male = ['Lionel', 'Wardell',  'Jamel', 'Marcellus',
+  'Jamal', 'Leroy',  'Tyrone', 'Alphonse'
+, 'Hakim', 'Terrence', 'Jerome', 'Alonzo'][0:12]
+#delete: Deion, Ebony, Kareem,Lamar,Lavon,Malik,Rasheed,Jermaine,
+# Tyree,Lamont,Darnell,Torrance,Theo
+
+european_female = ['Melanie','Colleen','Ellen','Emily','Sarah','Rachel',
+'Carrie','Stephanie','Megan','Nancy', 'Katie','Heather','Betsy',
+'Kristin'][0:12]
+#delete: Amanda
+
+european_male = ['Frank',   'Roger', 'Neil', 'Geoffrey',
+ 'Brad', 'Stephen', 'Peter', 'Josh', 'Andrew',  'Jack', 
+'Matthew', 'Jonathan',  'Greg',
+  'Justin', 'Alan',    'Adam',
+    'Harry',  'Paul'][0:12]
+#delete: Lauren,Jill,Brendan,Meredith,Allison,Todd,Ryan,Courtney,Laurie,Brett,Anne
+
+mexican_female = ['Maria','Yesenia','Adriana','Liset','Mayra','Alma',
+'Carolina','Iliana','Sonia',
+                    'Karina','Alejandra','Brenda','Vanessa','Diana'][0:12]
+# delete: Ana
+mexican_male = ['Jesús','Rigoberto','César','Rogelio','José','Pedro',
+'Antonio','Alberto','Alejandro',
+                  'Alfredo','Juan','Miguel','Ricardo'][0:12]
+
+af_bias = ['loud','ghetto','unrefined','athletic','unintelligent']+['bigbutt','overweight','confident','darkskinned','promiscuous','unfeminine','aggressive','chicken']
+em_bias_foraf = ['rich', 'intelligent', 'arrogant', 'status', 'blond', 'racist', 'American', 'leader', 'privileged', 'attractive', 'tall', 'sexist', 'successful']
+
+af_unique_bias = ['bigbutt','overweight','confident','darkskinned','promiscuous','unfeminine','aggressive','chicken']
+em_unique_bias_foraf = ['rich', 'tall', 'intelligent', 'arrogant', 'successful', 'status', 'blond', 'racist']
+
+lf_bias = ['feisty','curvy', 'loud',  'cook', 'darkskinned', 'uneducated', 'hardworker' ,'promiscuous','unintelligent','short','sexy', 'maids']
+em_bias_forlf = ['rich', 'intelligent', 'arrogant', 'status', 'blond', 'racist', 'American', 'leader', 'privileged',  'tall', 'sexist', 'successful']
+
+lf_unique_bias = ['feisty','curvy','cook','promiscuous','sexy','maids']
+em_unique_bias_forlf = ['rich', 'tall', 'intelligent', 'assertive', 'arrogant', 'successful']
+
+
+weat_groups = [
+[flowers,insects,pleasant,unpleasant], # 1
+[instruments, weapons, pleasant, unpleasant], #2
+[european_3,african_3,pleasant_3,unpleasant_3], #3
+[european_4,african_4,pleasant_3,unpleasant_3], #4
+[european_4,african_4,pleasant_5,unpleasant_5],#5
+[male,female,career,family], #6
+[math,arts,male_term,female_term],#7
+[science,arts_8,male_term_8,female_term_8],#8
+[mental_disease,physical_disease,temporary,permanent],#9
+[young_name,old_name,pleasant_5,unpleasant_5],#10
+    [african_female,european_male,af_bias,em_bias_foraf], #af-inter
+    [african_female,european_male,af_unique_bias,em_unique_bias_foraf], #af-emerg
+    [mexican_female,european_male,lf_bias,em_bias_forlf],#lf-inter
+    [mexican_female,european_male,lf_unique_bias,em_unique_bias_forlf]# lf-emerg
+]
+
+
+def associate(w,A,B):
+    return cosine_similarity(w.reshape(1,-1),A).mean() - cosine_similarity(w.reshape(1,-1),B).mean()
+
+def difference(X,Y,A,B):
+    # return np.sum(np.apply_along_axis(associate,1,X,A,B)) - np.sum(np.apply_along_axis(associate,1,Y,A,B))
+
+    return np.sum([associate(X[i,:],A,B) for i in range(X.shape[0])]) - np.sum([associate(Y[i,:],A,B) for i in range(Y.shape[0])])
+
+def effect_size(X,Y,A,B):
+    # delta_mean = np.mean(np.apply_along_axis(associate,1,X,A,B)) - np.mean(np.apply_along_axis(associate,1,Y),A,B)
+    delta_mean =  np.mean([associate(X[i,:],A,B) for i in range(X.shape[0])]) - np.mean([associate(Y[i,:],A,B) for i in range(Y.shape[0])])
+
+    # s = np.apply_along_axis(associate,1,np.concatenate((X,Y),axis=0),A,B)
+    XY = np.concatenate((X,Y),axis=0)
+    s = [associate(XY[i,:],A,B) for i in range(XY.shape[0])]
+
+    std_dev = np.std(s,ddof=1)
+    var = std_dev**2
+
+    return delta_mean/std_dev, var
+
+def inn(a_huge_key_list):
+    L = len(a_huge_key_list)
+    i = np.random.randint(0, L)
+    return a_huge_key_list[i]
+
+
+def sample_statistics(X,Y,A,B,num = 100):
+    XY = np.concatenate((X,Y),axis=0)
+   
+    def inner_1(XY,A,B):
+        X_test_idx = np.random.choice(XY.shape[0],X.shape[0],replace=False)
+        Y_test_idx = np.setdiff1d(list(range(XY.shape[0])),X_test_idx)
+        X_test = XY[X_test_idx,:]
+        Y_test = XY[Y_test_idx,:]
+        return difference(X_test,Y_test,A,B)
+    
+    s = [inner_1(XY,A,B) for i in range(num)]
+
+    return np.mean(s), np.std(s,ddof=1)
+
+def p_value(X,Y,A,B,num=100):
+    m,s = sample_statistics(X,Y,A,B,num)
+    d = difference(X,Y,A,B)
+    p = 1 - scipy.stats.norm.cdf(d,loc = m, scale = s)
+    return p
+
+def ceat_meta(weat_groups = weat_groups, model='bert',test=1,N=10000):
+    nm = "bertweat1.pickle".format(model)
+    weat_dict = pickle.load(open(nm,'rb'))
+    # nm_1 = "name_{}_vector_new.pickle".format(model)
+    # name_dict = pickle.load(open(nm_1,'rb'))  
+
+    e_lst = [] #effect size
+    v_lst = [] #variance
+
+    len_list = [len(weat_groups[test-1][i]) for i in range(4)]
+
+    for i in range(N):
+        X = np.array([weat_dict[wd][np.random.randint(0,len(weat_dict[wd]))] for wd in weat_groups[test-1][0]])
+        Y = np.array([weat_dict[wd][np.random.randint(0,len(weat_dict[wd]))] for wd in weat_groups[test-1][1]])
+        A = np.array([weat_dict[wd][np.random.randint(0,len(weat_dict[wd]))] for wd in weat_groups[test-1][2]])
+        B = np.array([weat_dict[wd][np.random.randint(0,len(weat_dict[wd]))] for wd in weat_groups[test-1][3]])
+        e,v = effect_size(X,Y,A,B)
+        e_lst.append(e)
+        v_lst.append(v)
+
+    # e_nm = "/Users/ceatpro/Desktop/wefat/data/meta_data/{0}_{1}_es.pickle".format(model,test)
+    # v_nm = "/Users/ceatpro/Desktop/wefat/data/meta_data/{0}_{1}_v.pickle".format(model,test)
+    # pickle.dump(e_lst,open(e_nm,'wb'))
+    # pickle.dump(v_lst,open(v_nm,'wb'))
+    
+    #calculate Q (total variance)
+    e_ary = np.array(e_lst)
+    w_ary = 1/np.array(v_lst)
+
+    q1 = np.sum(w_ary*(e_ary**2))
+    q2 = ((np.sum(e_ary*w_ary))**2)/np.sum(w_ary)
+    q = q1 - q2
+
+    df = N - 1
+
+    if q>df:
+        c = np.sum(w_ary) - np.sum(w_ary**2)/np.sum(w_ary)
+        tao_square = (q-df)/c
+        print("tao>0")
+    else:
+        tao_square = 0
+
+    v_ary = np.array(v_lst)
+    v_star_ary = v_ary + tao_square
+    w_star_ary = 1/v_star_ary
+
+    # calculate combiend effect size, variance
+    pes = np.sum(w_star_ary*e_ary)/np.sum(w_star_ary)
+    v = 1/np.sum(w_star_ary)
+
+    # p-value
+    z = pes/np.sqrt(v)
+    # p_value = 1 - scipy.stats.norm.cdf(z,loc = 0, scale = 1)
+    p_value = scipy.stats.norm.sf(z,loc = 0, scale = 1)
+
+
+    return pes, p_value
+
+
+if __name__ == '__main__':
+
+    e_lst = []
+    p_lst = []
+    for e in range(1,15):
+        # group = weat_groups[(e - 1)]
+        e_lst.append([])
+        p_lst.append([])
+        print(e)
+        for m in ["bert"]:
+            print(m)
+            print(datetime.datetime.now().strftime("%Y-%m-%d %H:%M"))
+
+            pes,  p_value = ceat_meta(weat_groups = weat_groups, model=m,test=e,N=1000)
+            print("PES is {}:".format(pes))
+            # print("Var is {}:".format(v))
+            print("P-value is {}:".format(p_value))
+            e_lst[e-1].append(pes)
+            # e_lst[e-1].append(v)
+            e_lst[e-1].append(p_value)
+            print(" ")
+    
+    e_ary = np.array(e_lst)
+    p_ary = np.array(p_lst)
+
+    np.savetxt("e_1000.csv", e_ary, delimiter=",")
