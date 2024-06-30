f5333d4fa17ffa4277ce7288bfdf06c57fe6a416
Notta <thang.dv509@gmail.com>
2024-06-30 17:48:44 +0700

    update

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import sys
 from models.bert import *
 from models.openaigpt import *
 from models.llama2 import *
-from intrinsic_bias.probability_based.weat import main as weat
-from intrinsic_bias.probability_based.seat import main as seat
-from intrinsic_bias.probability_based.ceat import main as ceat
+from medium_sized.intrinsic_bias.probability_based.weat import main as weat
+from medium_sized.intrinsic_bias.probability_based.seat import main as seat
+from medium_sized.intrinsic_bias.probability_based.ceat import main as ceat
 
 #weat.run_experiment()
